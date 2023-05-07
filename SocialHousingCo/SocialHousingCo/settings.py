ce82b8579e2d803d231cfe583ed06ab5b52d5986
itsMian <mianisme.01@gmail.com>
2023-05-07 22:04:05 +1000

    Quan - made responsive listpage and resultpage

--- SocialHousingCo/SocialHousingCo/settings.py
+++ SocialHousingCo/SocialHousingCo/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'SocialHousingCo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'userdb',
-        'USER': 'postgres',
-        'PASSWORD': 'SCCT1223',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
