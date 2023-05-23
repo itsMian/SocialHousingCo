41f47a562a5cc06ad442ce09e17c3adc589b3f1e
shayannearif2000 <shayannearif.arif@gmail.com>
2023-05-23 22:27:33 +1000

    updated create account page

--- SocialHousingCo/Login/views.py
+++ SocialHousingCo/Login/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-
-
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'login.html')
 
 
+
+
+
+
+
+
+
+@csrf_protect
+def signin(request):
+    if request.method == 'POST':
+        email = request.POST['Email']
+        password = request.POST['password']
+        
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/profiles')
+        else:
+            messages.info(request, 'Invalid username or password')
+            return redirect("/login")  # Add the return statement here
+    else:
+        return render(request, 'index.html')
