3a418f018e929ad50fc6e4b07fe89b752b446be0
itsMian <mianisme.01@gmail.com>
2023-05-24 21:35:59 +1000

    delete all folders and files for fixing multiple project

--- Home/static/Listpage/javascripts/map.js
+++
@@ -1,5 +0,0 @@
-const copy = "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
-const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const layer = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [layer] });
-map.fitWorld();
--- SocialHousingCo/Database/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Property
- 
-def showproperty(request):
-    properties = Property.objects
-    return render(request, 'property.html', { 'propertys': properties } )
