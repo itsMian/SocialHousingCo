ab24151a1b1f9d2017df24b805865460ed4a4e42
shayannearif2000 <shayannearif.arif@gmail.com>
2023-05-26 21:19:57 +1000

    updated rego page

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shayannearif/Documents/SocialHousingCo/.venv"
+set -gx VIRTUAL_ENV "/Users/shayannearif/SocialHousingCov0.1/SocialHousingCo/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
