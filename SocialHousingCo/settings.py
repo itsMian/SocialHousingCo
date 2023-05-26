f4d4da125bd3bf3e7dc65e2f89a44e64e2f4a496
19262563 <19262563@student.westernsydney.edu.au>
2023-05-26 17:38:26 +1000

    solved problem v1.3

--- SocialHousingCo/settings.py
+++ SocialHousingCo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'Listpage',
     'bootstrap5',
     'Database',
