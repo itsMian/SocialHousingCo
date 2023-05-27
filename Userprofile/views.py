b21300ab3cefabb646cf98031e4ff57dc47ee74c
Bojun8333 <bojunhou12138@gmail.com>
2023-05-27 19:39:08 +1000

    27/05 new change

--- Userprofile/views.py
+++ Userprofile/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import Profiles
+from .forms import IncomeForm
 
 
 def index(request):
@@ -19,3 +20,20 @@ def aboutme(request):
 def identity(request):
     return render(request, 'identity-documents.html')
 
+def income_create(request):
+    if request.method == 'POST':
+        form = IncomeForm(request.POST)
+        if form.is_valid():
+            # Assuming the user is logged in and the Profiles instance is linked to User
+            profile = Profiles.objects.get(user=request.user)
+            income = form.save(commit=False)
+            income.profile = profile
+            income.save()
+            return redirect('profiles')
+    else:
+        form = IncomeForm()
+    return render(request, 'income.html', {'form': form})
+
+
+def referal_letter(request):
+    return render(request, 'referal-letter.html')
