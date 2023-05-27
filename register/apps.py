a728624606ecc33328a541e30ac1ba272bf6325a
shayannearif2000 <shayannearif.arif@gmail.com>
2023-05-27 22:25:16 +1000

    functioning website with all views

--- register/apps.py
+++ register/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LoginConfig(AppConfig):
+class RegisterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Login'
+    name = 'register'
